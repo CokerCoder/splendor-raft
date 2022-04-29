d1a6b11fef06f03952d0486da384c7e51693eaf6
Tianlalu233 <562775064@qq.com>
2022-04-29 16:38:31 +0800

    Draft the RPC interface; Add .proto;

--- src/main/java/com/da/rpc/RPCAdapter.java
+++ src/main/java/com/da/rpc/RPCAdapter.java
@@ -1,28 +1,24 @@
 package com.da.rpc;
 
-import java.util.Collection;
-
-import com.da.entity.AppendEntriesRpc;
-import com.da.entity.AppendEntriesResult;
-import com.da.entity.RequestVoteRpc;
-import com.da.entity.RequestVoteResult;
+import com.da.entity.*;
 import com.da.node.Node;
 
-// 最终RPC服务转换成的适配器接口，RPC service暂时没有实现
-// 该类相当于lu-raft里的RpcClient，但是把可以发送的request拆分成了具体的（下面四个）
+
+/**
+ * 最终RPC服务转换成的适配器接口，RPC service暂时没有实现
+ */
 public interface RPCAdapter {
-    
-    void initialize();
 
-    // 这里的Node并不是地址类，需要新增Node地址类
-    void sendRequestVote(RequestVoteRpc request, Collection<Node> destinations);
+    // 开始监听端口
+    void listenOn(int port);
 
-    void replyRequestVote(RequestVoteResult result, Node destination);
+    // destination 类型待定（需要address和port)
+    // RPC 需调用 Node的 XXXResult handleXXX(XXXRpc)
+    RequestVoteResult requestVoteRPC(RequestVoteRpc request, Node destination);
 
-    void sendAppendEntries(AppendEntriesRpc request, Collection<Node> destinations);
+    AppendEntriesResult appendEntriesRPC(AppendEntriesRpc request, Node destination);
 
-    void replyAppendEntries(AppendEntriesResult result, Node destination);
-
+    // 关闭所有连接
     void close();
-    
+
 }
