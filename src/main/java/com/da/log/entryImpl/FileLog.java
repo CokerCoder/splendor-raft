1ab46c66716c9d0e7ea2643a33378e9ad65852d1
Yunfei Jing <yunfei.jing@outlook.com>
2022-05-04 23:34:09 +0800

    fixed FileLog

--- src/main/java/com/da/log/entryImpl/FileLog.java
+++ src/main/java/com/da/log/entryImpl/FileLog.java
@@ -9,6 +9,8 @@ import java.io.File;
 //p141
 public class FileLog extends AbstractLog{
 
+    private final RootDir rootDir;
+
     public FileLog(File baseDir){
         rootDir = new RootDir(baseDir); //baseDir 表示日志目录的根目录
         //获得最新日志代
@@ -21,21 +23,5 @@ public class FileLog extends AbstractLog{
             LogGeneration firstGeneration = rootDir.createFirstGeneration();
             entrySequence = new FileEntrySequence(firstGeneration, 1);
         }
-    @Override
-    public int getNextIndex() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public int getCommitIndex() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public void close() {
-        // TODO Auto-generated method stub
-        
     }
 }
